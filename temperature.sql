369d0cbfa2a66ea69e5f1346b591aefa51c9dd75
Roshan-31 <65827778+Roshan-31@users.noreply.github.com>
2020-12-23 00:24:12 +0530

    Update and rename temperature.db to temperature.sql

--- temperature.sql
+++ temperature.sql
@@ -8,7 +8,9 @@ CREATE TABLE `user_details` (
 CREATE TABLE `temp_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sensor` varchar(45) NOT NULL,
-  `temp_value` varchar(45) NOT NULL,
+  `temp_value` int(11) NOT NULL,
   `create_timestamp` timestamp NOT NULL,
   PRIMARY KEY (`id`)
 );
+UPDATE TABLE 'temp_details'(
+'temp_value'=15,'create_timestamp'=8:00,9:00 );
